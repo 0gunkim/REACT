3476470e6da68a586400636b58f182543525b7c9
Goldenprevue <goldenprevue@gmail.com>
2022-12-19 20:54:51 +0900

    상태값 변경하기 With promt

--- chapterfive/src/AppMentor.jsx
+++ chapterfive/src/AppMentor.jsx
@@ -3,15 +3,27 @@ import React, {useState} from 'react';
 export default function AppMentor() {
     const [person, setPerson] = useState({
         name: 'max',
-        title: 'developer',
+        title: '주니어 개발자',
         mentor: {
-            name: '엘리',
-            title: '시니어개발자',
+            name: 'bob',
+            title: '시니어 개발자',
         }
     })
+    // const changeHandler = person.mentor.title;
     return (
         <div>
-            
+            <h1>{person.name}는 {person.title}</h1>
+            <p>{person.name}의 멘토는 {person.mentor.name} ({person.mentor.title})</p>
+            <button onClick={()=>{
+                const name = prompt(`what's your mentor's name?`);
+                setPerson(person => ({...person, mentor: {...person, name}}))
+
+            }}>멘토 이름 바꾸기</button>
+            <button onClick={(e)=>{
+                const title = prompt(`what's your mentor's name?`, `title change`);
+                setPerson(person => ({...person, mentor: {...person, title}
+            }))
+            }}>멘토 타이틀 바꾸기</button>
         </div>
     );
 }
